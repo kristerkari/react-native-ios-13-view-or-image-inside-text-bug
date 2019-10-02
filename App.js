9be64bccc65f5814a74f3b37c6fa44cc77589497
krister <krister@sent.at>
2019-10-02 10:48:40 +0300

    Add test case for iOS 13 <View> or <Image> inside <Text> bug

--- App.js
+++ App.js
@@ -8,105 +8,71 @@
 
 import React from 'react';
 import {
+  Image,
   SafeAreaView,
-  ScrollView,
   StatusBar,
   StyleSheet,
   Text,
   View,
 } from 'react-native';
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
 const App: () => React$Node = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
+      <SafeAreaView style={styles.wrapper}>
+        <Text style={styles.text}>View</Text>
+        <View style={styles.round} />
+        <Text style={styles.text}>Image</Text>
+        <Image
+          style={styles.image}
+          source={{
+            uri:
+              'https://upload.wikimedia.org/wikipedia/commons/5/58/Ponto_Amarelo.png',
+          }}
+        />
+        <Text style={styles.text}>View inside Text</Text>
+        <Text>
+          <View style={styles.round} />
+        </Text>
+        <Text style={styles.text}>Image inside Text</Text>
+        <Text>
+          <Image
+            style={styles.image}
+            source={{
+              uri:
+                'https://upload.wikimedia.org/wikipedia/commons/5/58/Ponto_Amarelo.png',
+            }}
+          />
+        </Text>
       </SafeAreaView>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  wrapper: {
+    flex: 1,
+    backgroundColor: '#ccc',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  text: {
+    marginTop: 16,
+    marginBottom: 16,
+    fontSize: 20,
+    fontWeight: 'bold',
   },
-  body: {
-    backgroundColor: Colors.white,
+  round: {
+    width: 30,
+    height: 30,
+    backgroundColor: 'red',
+    borderRadius: 15,
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  image: {
+    width: 30,
+    height: 30,
   },
 });
 
